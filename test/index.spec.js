c62f6b1eccc79f187b0a238f28f8ea6134e8017c
hlays <lays.hidani@gmail.com>
2019-04-23 14:06:23 -0300

    add tests describe

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,28 +1,32 @@
 const { assert } = require('chai');
 const cpfValidator = require('../index');
 
-describe('cpfValidator()', () => {
-  it('Deve retornar true para CPF válidos: 12345678909', () => {
-    assert.equal(cpfValidator('12345678909'), true);
+describe('Validador de CPF', () => {
+  describe('Deve retornar true para CPFs válidos:', () => {
+    it('12345678909', () => {
+      assert.equal(cpfValidator('12345678909'), true);
+    });
+
+    it('33092858042', () => {
+      assert.equal(cpfValidator('33092858042'), true);
+    });
+
+    it('77266592038', () => {
+      assert.equal(cpfValidator('77266592038'), true);
+    });
   });
 
-  it('Deve retornar false para CPF: 33092858042', () => {
-    assert.equal(cpfValidator('33092858042'), true);
-  });
+  describe('Deve retornar false para CPFs inválidos:', () => {
+    it('vazio', () => {
+      assert.equal(cpfValidator(''), false);
+    });
 
-  it('Deve retornar false para CPF: 77266592038', () => {
-    assert.equal(cpfValidator('77266592038'), true);
-  });
+    it('< 9 dígitos: 123456', () => {
+      assert.equal(cpfValidator('123456'), false);
+    });
 
-  it('Deve retornar false para CPF vazio', () => {
-    assert.equal(cpfValidator(''), false);
-  });
-
-  it('Deve retornar false para CPF com menos de 9 dígitos', () => {
-    assert.equal(cpfValidator('123456'), false);
-  });
-
-  it('Deve retornar false para CPF com 9 dígitos iguais como 00000000000', () => {
-    assert.equal(cpfValidator('000000000'), false);
+    it('9 dígitos iguais como 00000000000', () => {
+      assert.equal(cpfValidator('000000000'), false);
+    });
   });
 });
