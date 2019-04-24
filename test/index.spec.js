64b99a843da810375ceebad16c3e1f935f7a08ff
hlays <lays.hidani@gmail.com>
2019-04-24 17:21:10 -0300

    add more test cases

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,20 +3,20 @@ const cpfValidator = require('../index');
 
 describe('Validador de CPF', () => {
   describe('Deve retornar true para CPFs válidos:', () => {
-    it('12345678909', () => {
+    it('resto =  0: 82367085005', () => {
+      assert.equal(cpfValidator.cpfValidator('82367085005'), true);
+    });
+    it('resto = 1: 36161186039', () => {
+      assert.equal(cpfValidator.cpfValidator('36161186039'), true);
+    });
+    it('resto = 5: 32384739028', () => {
+      assert.equal(cpfValidator.cpfValidator('32384739028'), false);
+    });
+    it('resto = 10: 12345678909', () => {
       assert.equal(cpfValidator.cpfValidator('12345678909'), true);
     });
-
-    it('33092858042', () => {
-      assert.equal(cpfValidator.cpfValidator('33092858042'), true);
-    });
-
-    it('77266592038', () => {
-      assert.equal(cpfValidator.cpfValidator('77266592038'), true);
-    });
-
-    it('32384739028', () => {
-      assert.equal(cpfValidator.cpfValidator('32384739028'), false);
+    it('resto !== do primeiro dígito verificador e resto < 2: 97509998000', () => {
+      assert.equal(cpfValidator.cpfValidator('97509998000'), true);
     });
   });
 
